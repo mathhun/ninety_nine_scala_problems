25dead78e156e8fc8cb3af73ae8d64f26ab31b4e
mathhun <achemer@gmail.com>
2015-08-28 15:53:23 +0900

    #4

--- src/main/scala/problem/main.scala
+++ src/main/scala/problem/main.scala
@@ -1,23 +1,36 @@
 package problem
 
 object Main {
-  /*
-   * P01 (*) Find the last element of a list.
-   * Example:
-   * scala> last(List(1, 1, 2, 3, 5, 8))
-   * res0: Int = 8
-   */
+   // P01 (*) Find the last element of a list.
+   // Example:
+   // scala> last(List(1, 1, 2, 3, 5, 8))
+   // res0: Int = 8
   def last[A](list: List[A]): A = {
     list.last
   }
 
-  /*
-   * P02 (*) Find the last but one element of a list.
-   * Example:
-   * scala> penultimate(List(1, 1, 2, 3, 5, 8))
-   * res0: Int = 5
-   */
+   // P02 (*) Find the last but one element of a list.
+   // Example:
+   // scala> penultimate(List(1, 1, 2, 3, 5, 8))
+   // res0: Int = 5
   def penultimate[A](list: List[A]): A = {
     list.takeRight(2).head
+  }
+
+   // P03 (*) Find the Kth element of a list.
+   // By convention, the first element in the list is element 0.
+   // Example:
+   // scala> nth(2, List(1, 1, 2, 3, 5, 8))
+   // res0: Int = 2
+  def nth[A](n: Int, list: List[A]): A = {
+    list.take(n + 1).last
+  }
+
+  // P04 (*) Find the number of elements of a list.
+  // Example:
+  // scala> length(List(1, 1, 2, 3, 5, 8))
+  // res0: Int = 6
+  def length[A](list: List[A]): Int = {
+    list.length
   }
 }
--- src/test/scala/problem/MainSpec.scala
+++ src/test/scala/problem/MainSpec.scala
@@ -12,3 +12,15 @@ class P02Spec extends FlatSpec with Matchers {
     penultimate(List(1, 1, 2, 3, 5, 8)) should be (5)
   }
 }
+
+class P03Spec extends FlatSpec with Matchers {
+  "nth" should "find the Kth element of a list" in {
+    nth(2, List(1, 1, 2, 3, 5, 8)) should be (2)
+  }
+}
+
+class P04Spec extends FlatSpec with Matchers {
+  "length" should "find the number of elements of a list" in {
+    problem.Main.length(List(1, 1, 2, 3, 5, 8)) should be (6)
+  }
+}
