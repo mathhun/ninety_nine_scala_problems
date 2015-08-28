d5247b6d6b96e908cf5f9ead21a18ef7239609ee
mathhun <achemer@gmail.com>
2015-08-28 17:41:48 +0900

    cleanup

--- src/main/scala/problem/main.scala
+++ src/main/scala/problem/main.scala
@@ -5,34 +5,30 @@ object Main {
    // Example:
    // scala> last(List(1, 1, 2, 3, 5, 8))
    // res0: Int = 8
-  def last[A](list: List[A]): A = {
+  def last[A](list: List[A]): A =
     list.last
-  }
 
    // P02 (*) Find the last but one element of a list.
    // Example:
    // scala> penultimate(List(1, 1, 2, 3, 5, 8))
    // res0: Int = 5
-  def penultimate[A](list: List[A]): A = {
+  def penultimate[A](list: List[A]): A =
     list.takeRight(2).head
-  }
 
    // P03 (*) Find the Kth element of a list.
    // By convention, the first element in the list is element 0.
    // Example:
    // scala> nth(2, List(1, 1, 2, 3, 5, 8))
    // res0: Int = 2
-  def nth[A](n: Int, list: List[A]): A = {
+  def nth[A](n: Int, list: List[A]): A =
     list(n)
-  }
 
   // P04 (*) Find the number of elements of a list.
   // Example:
   // scala> length(List(1, 1, 2, 3, 5, 8))
   // res0: Int = 6
-  def length[A](list: List[A]): Int = {
+  def length[A](list: List[A]): Int =
     list.length
-  }
 
   // P05 (*) Reverse a list.
   // Example:
