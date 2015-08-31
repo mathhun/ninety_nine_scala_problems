924da6e2ba3f417bc24e3298f7868d5c2cda001b
mathhun <achemer@gmail.com>
2015-08-31 18:42:09 +0900

    Problem #9

--- src/test/scala/problem/MainSpec.scala
+++ src/test/scala/problem/MainSpec.scala
@@ -47,6 +47,15 @@ class P07Spec extends FlatSpec with Matchers {
 
 class P08Spec extends FlatSpec with Matchers {
   "compress" should "eliminate consecutive duplicates of list elements" in {
-    compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be (List('a, 'b, 'c, 'a, 'd, 'e))
+    val xs = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    compress(xs) should be (List('a, 'b, 'c, 'a, 'd, 'e))
+  }
+}
+
+class P09Spec extends FlatSpec with Matchers {
+  "pack" should "pack consecutive duplicates of list elements into sublists" in {
+    val xs = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    val ys = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+    pack(xs) should be (ys)
   }
 }
