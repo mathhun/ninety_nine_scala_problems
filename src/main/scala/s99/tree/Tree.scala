b490a86fbcedb1d5b20a4f8d9aa34eff2a9fe4b2
mathhun <achemer@gmail.com>
2015-11-10 22:46:43 +0900

    #56

--- src/main/scala/s99/tree/Tree.scala
+++ src/main/scala/s99/tree/Tree.scala
@@ -1,11 +1,21 @@
 package s99.tree
 
-sealed abstract class Tree[+T]
+sealed abstract class Tree[+T] {
+  def isSymmetric: Boolean = this match {
+    case End => true
+    case Node(_, l, r) => l.isMirrorOf(r)
+  }
+
+  def isMirrorOf[T](that: Tree[T]): Boolean = (this, that) match {
+    case (End, End) => true
+    case (End, _) => false
+    case (_, End) => false
+    case (Node(_, l0, r0), Node(_, l1, r1)) => l0.isMirrorOf(l1) && r0.isMirrorOf(r1)
+  }
+}
 
 case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
   override def toString = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
-
-  def isSymmetric: Boolean = true
 }
 
 case object End extends Tree[Nothing] {
--- src/test/scala/s99/tree/TreeSpec.scala
+++ src/test/scala/s99/tree/TreeSpec.scala
@@ -1,6 +1,7 @@
 package s99.tree
 
 import org.scalatest._
+import org.scalatest.prop.TableDrivenPropertyChecks._
 import s99.tree.Tree._
 
 class P55Spec extends FlatSpec with Matchers {
@@ -37,14 +38,24 @@ class P56Spec extends FunSpec with Matchers {
         Node().isSymmetric should be (true)
       }
     }
-    describe("when trees are symmetric") {
-      it("should be true") {
-        Node('a', Node('b'), Node('c')).isSymmetric should be (true)
-      }
-    }
-    describe("when trees are not symmetric") {
-      it("should be false") {
-        Node('a', Node('b'), End).isSymmetric should be (false)
+    it("should be true") {
+      val cases = Table(
+        ("Tree", "ExpectedValue"),
+        // true
+        (Node('a', Node('b'), Node('c')), true),
+        (Node('a',
+          Node('b', Node('d', Node('e'), End)),
+          Node('c', Node('f', Node('g'), End))), true),
+
+        // false
+        (Node('a', Node('b'), End), false),
+        (Node('a', End, Node('b')), false),
+        (Node('a',
+          Node('b', Node('d'), End),
+          Node('c', End, Node('e'))), false)
+      )
+      forAll (cases) { (tree, expected) =>
+        tree.isSymmetric should be (expected)
       }
     }
   }
