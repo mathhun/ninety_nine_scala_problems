ad9928e6022879c33e739401323ae9b476d5fb43
mathhun <achemer@gmail.com>
2015-11-28 23:22:29 +0900

    #70

--- src/main/scala/s99/tree/MTree.scala
+++ src/main/scala/s99/tree/MTree.scala
@@ -1,12 +1,32 @@
 package s99.tree
 
-class MTree[+T](value: T, children: List[MTree[T]]) {
-  override def toString = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
+class MTree[+T](val value: T, val children: List[MTree[T]]) {
+  //override def toString = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
+  override def toString = value.toString + children.map(_.toString + "^").mkString("")
 
   //def nodeCount: Int = 1 + children.map(_.nodeCount).sum
   def nodeCount: Int = children.foldLeft(1)(_ + _.nodeCount)
+
+  override def equals(o: Any) = o match {
+    case that: MTree[T] => value == that.value && children == that.children
+    case _ => false
+  }
 }
 
 object MTree {
   def apply[T](value: T, children: List[MTree[T]] = Nil) = new MTree(value, children)
+
+  implicit def string2MTree(s: String): MTree[Char] = {
+    def nextStrBound(pos: Int, nesting: Int): Int = {
+      if (nesting == 0) pos
+      else nextStrBound(pos + 1, if (s(pos) == '^') nesting - 1 else nesting + 1)
+    }
+    def splitChildStrings(pos: Int): List[String] =
+      if (pos + 1 >= s.length) Nil
+      else {
+        val end = nextStrBound(pos + 1, 1)
+        s.substring(pos, end - 1) :: splitChildStrings(end)
+      }
+    MTree(s(0), splitChildStrings(1).map(string2MTree(_)))
+  }
 }
--- src/test/scala/s99/tree/MTreeSpec.scala
+++ src/test/scala/s99/tree/MTreeSpec.scala
@@ -8,5 +8,27 @@ class P70CSpec extends FunSpec with Matchers {
   it("Count the nodes of a multiway tree") {
     MTree('a', List(MTree('f'))).nodeCount should be (2)
     MTree('a', List(MTree('a'), MTree('b'))).nodeCount should be (3)
+
+  }
+}
+
+class P70Spec extends FunSpec with Matchers {
+  //    a
+  //  / | \
+  // f  c  b
+  // |    / \
+  // g   d   e
+  val mtree0 = MTree('a', List(
+    MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))
+  )
+
+  describe("Tree construction from a node string") {
+    it("string2MTree") {
+      MTree.string2MTree("a") should be (MTree('a', Nil))
+      MTree.string2MTree("afg^^c^bd^e^^^") should be (mtree0)
+    }
+    it("toString") {
+      mtree0.toString should be ("afg^^c^bd^e^^")
+    }
   }
 }
