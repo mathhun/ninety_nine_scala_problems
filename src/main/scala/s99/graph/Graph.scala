3185f3bf0531d3079c7f2d1774e0b0072f5e4d71
mathhun <achemer@gmail.com>
2015-12-01 23:24:02 +0900

    snapshot

--- src/main/scala/s99/graph/Graph.scala
+++ src/main/scala/s99/graph/Graph.scala
@@ -4,6 +4,7 @@ abstract class GraphBase[T, U] {
   case class Edge(n1: Node, n2: Node, value: U) {
     def toTuple = (n1.value, n2.value, value)
   }
+
   case class Node(value: T) {
     var adj: List[Edge] = Nil
     // neighbors are all nodes adjacent to this node.
@@ -12,7 +13,7 @@ abstract class GraphBase[T, U] {
 
   var nodes: Map[T, Node] = Map()
   var edges: List[Edge] = Nil
-  
+
   // If the edge E connects N to another node, returns the other node,
   // otherwise returns None.
   def edgeTarget(e: Edge, n: Node): Option[Node]
@@ -31,7 +32,7 @@ abstract class GraphBase[T, U] {
 
 class Graph[T, U] extends GraphBase[T, U] {
   override def equals(o: Any) = o match {
-    case g: Graph[_,_] => super.equals(g)
+    case g: Graph[_, _] => super.equals(g)
     case _ => false
   }
 
@@ -50,7 +51,7 @@ class Graph[T, U] extends GraphBase[T, U] {
 
 class Digraph[T, U] extends GraphBase[T, U] {
   override def equals(o: Any) = o match {
-    case g: Digraph[_,_] => super.equals(g)
+    case g: Digraph[_, _] => super.equals(g)
     case _ => false
   }
 
@@ -67,28 +68,35 @@ class Digraph[T, U] extends GraphBase[T, U] {
 
 abstract class GraphObjBase {
   type GraphClass[T, U]
+
   def addLabel[T](edges: List[(T, T)]) =
     edges.map(v => (v._1, v._2, ()))
-  def term[T](nodes: List[T], edges: List[(T,T)]) =
+
+  def term[T](nodes: List[T], edges: List[(T, T)]) =
     termLabel(nodes, addLabel(edges))
-  def termLabel[T, U](nodes: List[T], edges: List[(T,T,U)]): GraphClass[T, U]
+
+  def termLabel[T, U](nodes: List[T], edges: List[(T, T, U)]): GraphClass[T, U]
+
   def addAdjacentLabel[T](nodes: List[(T, List[T])]) =
     nodes.map(a => (a._1, a._2.map((_, ()))))
+
   def adjacent[T](nodes: List[(T, List[T])]) =
     adjacentLabel(addAdjacentLabel(nodes))
-  def adjacentLabel[T, U](nodes: List[(T, List[(T,U)])]): GraphClass[T, U]
+
+  def adjacentLabel[T, U](nodes: List[(T, List[(T, U)])]): GraphClass[T, U]
 }
 
 object Graph extends GraphObjBase {
   type GraphClass[T, U] = Graph[T, U]
 
-  def termLabel[T, U](nodes: List[T], edges: List[(T,T,U)]) = {
+  def termLabel[T, U](nodes: List[T], edges: List[(T, T, U)]) = {
     val g = new Graph[T, U]
     nodes.map(g.addNode)
     edges.map(v => g.addEdge(v._1, v._2, v._3))
     g
   }
-  def adjacentLabel[T, U](nodes: List[(T, List[(T,U)])]) = {
+
+  def adjacentLabel[T, U](nodes: List[(T, List[(T, U)])]) = {
     val g = new Graph[T, U]
     for ((v, a) <- nodes) g.addNode(v)
     for ((n1, a) <- nodes; (n2, l) <- a) {
@@ -98,23 +106,36 @@ object Graph extends GraphObjBase {
     g
   }
 
-  def fromString[T, U](s: String): Graph[T, U] = {
-  }
+  // def term[T, U](nodess: List[T], edges: List[(T, T)]): Graph[T, U] =
+  //   ???
+
+  // def adjacent[T, U](nodeAndNeighbors: List[(T, List[(T, T)])]): Graph[T, U] =
+  //   ???
+
+  // def fromString[T, U](s: String): Graph[T, U] = {
+  // }
 }
 
 object Digraph extends GraphObjBase {
   type GraphClass[T, U] = Digraph[T, U]
 
-  def termLabel[T, U](nodes: List[T], edges: List[(T,T,U)]) = {
+  def termLabel[T, U](nodes: List[T], edges: List[(T, T, U)]) = {
     val g = new Digraph[T, U]
     nodes.map(g.addNode)
     edges.map(v => g.addArc(v._1, v._2, v._3))
     g
   }
-  def adjacentLabel[T, U](nodes: List[(T, List[(T,U)])]) = {
+
+  def adjacentLabel[T, U](nodes: List[(T, List[(T, U)])]) = {
     val g = new Digraph[T, U]
     for ((n, a) <- nodes) g.addNode(n)
     for ((s, a) <- nodes; (d, l) <- a) g.addArc(s, d, l)
     g
   }
+
+  // def term[T, U](nodes: List[T], edges: List[(T, T)]): Digraph[T, U] =
+  //   ???
+
+  // def adjacent[T, U](nodeAndNeighbors: List[(T, T)]): Digraph[T, U] =
+  //   ???
 }
--- src/test/scala/s99/graph/GraphSpec.scala
+++ src/test/scala/s99/graph/GraphSpec.scala
@@ -1,8 +1,8 @@
-package s99.tree
+package s99.graph
 
 import org.scalatest._
 import org.scalatest.prop.TableDrivenPropertyChecks._
-import s99.graph
+import s99.graph._
 
 // g b - c
 // |  \ /
@@ -11,9 +11,59 @@ import s99.graph
 // d   k
 
 class P80CSpec extends FunSpec with Matchers {
+  // graph-term form
+  val g0 = Graph.term(
+    List('b', 'c', 'd', 'f', 'g', 'h', 'k'),
+    List(('b', 'c'), ('b', 'f'), ('c', 'f'), ('f', 'k'), ('g', 'h'))
+  )
+
+  // adjacency-list form
+  val g1 = Graph.adjacent(List(
+    ('b', List('c', 'f')), ('c', List('b', 'f')), ('d', Nil),
+    ('f', List('b', 'c', 'k')), ('g', List('h')), ('h', List('g')),
+    ('k', List('f'))
+  ))
+
+  it("g0 == g1") {
+    pending
+  }
+
+  // human-readable form
+  // [b-c, f-c, g-h, d, f-b, k-f, h-g]
+
+  // graph-term form
+  val g2 = Digraph.term(
+    List('r', 's', 't', 'u', 'v'),
+    List(('s', 'r'), ('s', 'u'), ('u', 'r'), ('u', 's'), ('v', 'u'))
+  )
+
+  // adjacency-list form
+  val g3 = Digraph.adjacent(List(
+    ('r', Nil), ('s', List('r', 'u')), ('t', Nil),
+    ('u', List('r', 's')), ('v', List('u')))
+  )
+
+  // human-readable form
+  // [s>r, t, u>r, s>u, u>s, v>u]
+
+  // graph-term form
+  val g4 = Digraph.termLabel(
+    List('k', 'm', 'p', 'q'),
+    List(('m', 'q', 7), ('p', 'm', 5), ('p', 'q', 9))
+  )
+
+  // adjacency-list form
+  val g5 = Digraph.adjacentLabel(List(
+    ('k', Nil), ('m', List(('q', 7))), ('p', List(('m', 5), ('q', 9))), ('q', Nil)
+  ))
+
+  // human-readable form
+  // [p>q/9, m>q/7, k, p>m/5]
+
   it("fromString") {
-    Graph.fromString("[b-c, f-c, g-h, d, f-b, k-f, h-g]").toTermForm should be (
-      List(d, k, h, c, f, g, b),List((h,g,()), (k,f,()), (f,b,()), (g,h,()), (f,c,()), (b,c,()))
-    )
+    pending
+    // Graph.fromString("[b-c, f-c, g-h, d, f-b, k-f, h-g]").toTermForm should be (
+    //   List(d, k, h, c, f, g, b),List((h,g,()), (k,f,()), (f,b,()), (g,h,()), (f,c,()), (b,c,()))
+    // )
   }
 }
